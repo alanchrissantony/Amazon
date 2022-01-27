39a5bc45464cce41330c4b5cd52590846d363e48
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-01-27 14:27:21 +0530

    cart map

--- frontend/src/Components/Users/Cart/cart.jsx
+++ frontend/src/Components/Users/Cart/cart.jsx
@@ -1,54 +1,103 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import './cart.css'
+import LoadingBox from '../LoadingBox/loadingBox';
+import MessageBox from '../MessageBox/messageBox';
 
 
-function cart() {
-  return (
-      <div className='cartSection'>
-          <div className="cartContainer">
-              <div className="cartBoxContainer">
-                  <p className='shoppingCartText'>Shopping Cart</p>
-                  <p className='CartBoxPriceTitleText'>price</p>
-                  <hr className='cartTitleHr' />
-                <div className="cartBox">
-                    <div className="cartImageDiv">
-                        <img className='cartImage' src="https://www.gizmochina.com/wp-content/uploads/2021/07/noname-500x500.jpg" alt="" />
-                    </div>
-                    <div className="cartContentBox">
-                        <div className="cartProductTitleDiv">
-                            <p className="cartProductTitle">Acer Aspire 5 A515-56-36UT Slim Laptop | 15.6" Full HD Display | 11th Gen Intel Core i3-1115G4 Processor | 4GB DDR4 | 128GB NVMe SSD | WiFi 6 | Amazon Alexa |</p>
-                        </div>
-                        <div className="cartStockDiv">
-                            <p className='cartStockDiv'>In Stock</p>
-                        </div>
-                        <div className="cartQtyBtnDiv">
-                            <button className='cartBtnQty'>
-                                <span className='cartBtnText'>Qty: </span>
-                                <select className='cartSelectBtnQty' >
-                                    
-                                        <option >1</option>
+function Cart() {
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [cartQty, setCartQty] = useState(1);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            try {
+                setLoading(true)
+                const { data } = await axios.get('/api/products')
+                setLoading(false)
+                setProducts(data)
+            } catch (err) {
+                setError(err.message)
+                setLoading(false)
+            }
+        };
+        fetchData();
+    }, [])
+
+
+
+
+    return (
+        <div className='cartSection'>
+            {loading ? (
+                <LoadingBox></LoadingBox>
+            ) : error ? (
+                <MessageBox>{error}</MessageBox>
+            ) : (
+                <div className="cartContainer">
+                    <div className="cartBoxContainer">
+                        <p className='shoppingCartText'>Shopping Cart</p>
+                        <p className='CartBoxPriceTitleText'>price</p>
+                        <hr className='cartTitleHr' />
+                        {products.map(product=>(
+                            <div key={product._id} className="cartBox">
+                            <div className="cartImageDiv">
+                                <img className='cartImage' src={product.image} alt="" />
+                            </div>
+                            <div className="cartContentBox">
+                                <div className="cartProductTitleDiv">
+                                    <p className="cartProductTitle">{product.title}</p>
+                                </div>
+                                <div className="cartStockDiv">
+                                    {product.stock ? <p className="cartStockText">In Stock</p> : <p className="productScreenUnavailableText">Out of Stock</p>}
+                                </div>
+                                {product.stock ? <div className="cartQtyBtnDiv">
+                                    <button className='cartBtnQty'>
+                                        <span className='cartBtnText'>Qty: </span>
+                                        <select className='cartSelectBtnQty' value={2} onClick={(e)=>setCartQty(e.target.value)} >
+
+                                        {[...Array(product.stock)].map((x, i)=>
+                                        <option key={i+1}>{i+1}</option>
                                         
-                                    
-                                </select>
-                            </button>
+                                    )}
+
+
+                                        </select>
+                                    </button>
+                                </div> : ''}
+                                <div className="cartDeleteDiv">
+
+                                    <p className="cartDeleteTextTag"><span className='cartDeleteTextMark'>|</span><span className='cartDeleteText'>Delete</span></p>
+                                </div>
+
+                            </div>
+                            <div className="cartProductPriceDiv">
+                                <p className="cartProductPrice">${product.price}.{product.decimal}</p>
+                            </div>
+                            <hr className='cartBottomHr' />
                         </div>
-                        <div className="cartDeleteDiv">
-                            
-                            <p className="cartDeleteTextTag"><span className='cartDeleteTextMark'>|</span><span className='cartDeleteText'>Delete</span></p>
+                        ))}
+
+                        
+                        <div className="subTotalCartDiv">
+                            <p className="subTotalCartText"><span className='cartSubTotalText'>Subtotal (1 item) : </span><span className='cartSubTotalPrice'> $1356.99</span></p>
                         </div>
-            
                     </div>
-                    <div className="cartProductPriceDiv">
-                        <p className="cartProductPrice">$1345.99</p>
+                    <div className="checkoutBox">
+                        <p className="subTotalText"><span className='checkOutSubTotalText'>Subtotal (1 item) : </span><span className='checkOutSubTotalPrice'> $1356.99</span></p>
+                        <button className='checkOutBtn'>Proceed to checkout</button>
                     </div>
                 </div>
-              </div>
-              <div className="checkoutBox">
-                  
-              </div>
-          </div>
-      </div>
-  )
+            )}
+
+        </div>
+    )
 }
 
-export default cart;
+export default Cart;
