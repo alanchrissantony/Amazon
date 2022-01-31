aab1e7b0d4c6d3968582903184812bebfd41d918
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-01-31 21:37:42 +0530

    mongoose

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -2,6 +2,8 @@ import express from "express";
 import data from "../Data/data.js";
 import User from "../models/userModel.js";
 import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/utils.js";
 
 const userRouter = express.Router();
 
@@ -11,6 +13,27 @@ userRouter.get(
       // await User.remove({});
       const createdUsers = await User.insertMany(data.users);
       res.send({ createdUsers });
+    })
+  );
+
+
+  userRouter.post(
+    '/login',
+    expressAsyncHandler(async (req, res) => {
+      const user = await User.findOne({ email: req.body.email });
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          res.send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user),
+          });
+          return;
+        }
+      }
+      res.status(401).send({ message: 'Invalid email or password' });
     })
   );
 
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
 import express from 'express'
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 import data from './Data/data.js'
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js'
 
+
+dotenv.config();
+
 const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazon', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-app.get('/api/user', userRouter)
+app.get('/api/users', userRouter)
+app.get('/api/products', productRouter)
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
