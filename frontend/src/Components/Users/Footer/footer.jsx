92471263fc7990d98bc3750c581b5228d0b8babd
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-02-18 01:39:00 +0530

    foot scroll function

--- frontend/src/Components/Users/Footer/footer.jsx
+++ frontend/src/Components/Users/Footer/footer.jsx
@@ -7,19 +7,25 @@ import logo from '../../../Images/logo.png'
 function footer() {
 
   var day = new Date(),
-        date = day.getFullYear();
+    date = day.getFullYear();
 
   return (
-      <div>
-          <footer className='footerSection'>
-            <div className="row footerLogoDiv">
-                <img className='footerLogo' src={logo} alt="" />
-            </div>
-            <div className="row footerContentDiv">
-                <p className='footerContentText'>© 1996-{date}, Amazon.com, Inc. or its affiliates</p>
-            </div>
-          </footer>
-      </div>
+    <div>
+      <section className="footerBtnSec">
+        <button className="footerBtn" onClick={(e) => {
+          e.preventDefault()
+          window.scrollTo(0, 0)
+        }}>Back to top</button>
+      </section>
+      <footer className='footerSection'>
+        <div className="row footerLogoDiv">
+          <img className='footerLogo' src={logo} alt="" />
+        </div>
+        <div className="row footerContentDiv">
+          <p className='footerContentText'>© 1996-{date}, Amazon.com, Inc. or its affiliates</p>
+        </div>
+      </footer>
+    </div>
   )
 }
 
