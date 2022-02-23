45b367b841aa307cd9ee38ee9adc6b5b7b0777c1
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-02-24 00:41:41 +0530

    admin panel order activity design

--- frontend/src/Components/Admin/Panel/panel.jsx
+++ frontend/src/Components/Admin/Panel/panel.jsx
@@ -265,6 +265,79 @@ function AdminPanel() {
               </div>
             </div>
           </div>
+          <div className="adminPanelOrderActivitySection">
+            <div className="adminPanelOrderActivityContainer">
+              <div className="adminPanelOrderActivityDiv">
+                <div className="adminPanelOrderActivityBox">
+                  <div className="adminPanelOrderActivityBoxContainer">
+                    <div className="adminPanelOrderActivityBoxTitleContainer">
+                      <div className="adminPanelOrderActivityBoxTitleDiv">
+                        <p className="adminPanelOrderActivityBoxTitle">
+                          Order Activity
+                        </p>
+                      </div>
+                    </div>
+                    <div className="adminPanelOrderActivityBoxContentContainer">
+                      <div className="adminPanelOrderActivityBoxContentDiv">
+                        <div className="adminPanelOrderActivityBoxContentUserImgContainer">
+                          <div className="adminPanelOrderActivityBoxContentUserImgDiv">
+                            <img
+                              src="https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                              alt=""
+                              className="adminPanelOrderActivityBoxContentUserImg"
+                            />
+                          </div>
+                        </div>
+                        <div className="adminPanelOrderActivityBoxContentUserContentSection">
+                          <div className="adminPanelOrderActivityBoxContentUserContentContainer">
+                            <div className="adminPanelOrderActivityBoxContentUserContentDiv">
+                              <div className="adminPanelOrderActivityBoxContentUserNameDiv">
+                                <p className="adminPanelOrderActivityBoxContentUserName">
+                                  Lottie Arnold
+                                </p>
+                              </div>
+                              <div className="adminPanelOrderActivityBoxContentUserOrderTimeDiv">
+                                <p className="adminPanelOrderActivityBoxContentUserOrderTime">
+                                  25 mins ago
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="adminPanelOrderActivityBoxContentOrderContentContainer">
+                          <div className="adminPanelOrderActivityBoxContentOrderImgContainer">
+                            <div className="adminPanelOrderActivityBoxContentOrderImgDiv">
+                              <img
+                                src={dashProductImg}
+                                alt=""
+                                className="adminPanelOrderActivityBoxContentOrderImg"
+                              />
+                            </div>
+                          </div>
+                          <div className="adminPanelOrderActivityBoxContentOrderContentContainer">
+                            <div className="adminPanelOrderActivityBoxContentOrderContentDiv">
+                              <div className="adminPanelOrderActivityBoxContentOrderContentNameDiv">
+                                <p className="adminPanelOrderActivityBoxContentOrderContentName">
+                                  Iphone X Mobile
+                                </p>
+                              </div>
+                              <div className="adminPanelOrderActivityBoxContentOrderContentOrderCategoryDiv">
+                                <p className="adminPanelOrderActivityBoxContentOrderContentOrderCategory">
+                                  USB, wireless
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="adminPanelTopSellingProductSection">
             <div className="adminPanelTopSellingProductContainer">
               <div className="adminPanelTopSellingProductDiv">
@@ -288,6 +361,7 @@ function AdminPanel() {
                             />
                           </div>
                         </div>
+
                         <div className="adminPanelTopSellingProductContentTextContainer">
                           <div className="adminPanelTopSellingProductContentTextDiv">
                             <div className="adminPanelTopSellingProductContentTextTitleDiv">
@@ -370,7 +444,6 @@ function AdminPanel() {
                         </div>
                       </div>
                     </div>
-
                   </div>
                 </div>
               </div>
