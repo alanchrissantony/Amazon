661a26ffe2e1ec421267b52bba1ec8219163f5a4
Alan Chriss Antony <alanchrissantony@gmail.com>
2022-03-03 00:17:29 +0530

    banner

--- frontend/src/Components/Users/Departments/departments.jsx
+++ frontend/src/Components/Users/Departments/departments.jsx
@@ -38,7 +38,10 @@ function Departments() {
         ) : error ? (
           <MessageBox>{error}</MessageBox>
         ) : (
+          <div>
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" className='departmentBackgroundImg' alt="" />
           <section className='departmentsSectionContainer'>
+            
             <div className="departmentsDivContainer">
 
               {departments.map(departments=>(
@@ -46,6 +49,7 @@ function Departments() {
               ))}
             </div>
           </section>
+          </div>
           )}
       </div>
   )
